26208fda4cb348152c642545ae664eb071577a57
Waratnan Suriyasorn <s.waratnan@gmail.com>
2016-06-22 18:53:52 +0700

    Initial Commit

---
+++ MVVM_Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVM_Tutorial
+//
+//  Created by Waratnan Suriyasorn on 6/22/2559 BE.
+//  Copyright © 2559 Waratnan Suriyasorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
